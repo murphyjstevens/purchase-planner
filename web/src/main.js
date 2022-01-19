f97b4824dadb76eb0a27f697521a9c15b5e23bf0
Murphy Stevens <murphyjstevens@fastmail.com>
2022-01-18 19:54:43 -0600

    Updated to use bootstrap and normalize css

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,12 @@ import store from './store'
 
 import App from './App.vue'
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import 'normalize.css/normalize.css'
+import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 import './assets/global.scss'
 import './assets/bootstrap-override.css'
 
